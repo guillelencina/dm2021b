6e2c7f07f14dc66fcf23c1a887ccbf13d64aac22
GuilleLencina <galencina@gmail.com>
2021-10-07 14:44:51 -0300

    exp24 811y 822

--- src/avanzado/811_dataset_epic.r
+++ src/avanzado/811_dataset_epic.r
@@ -22,24 +22,24 @@ setwd( directory.root )
 
 palancas  <- list()  #variable con las palancas para activar/desactivar
 
-palancas$version  <- "v23"   #Muy importante, ir cambiando la version
+palancas$version  <- "v24"   #Muy importante, ir cambiando la version
 
 palancas$variablesdrift  <- c()   #aqui van las columnas que se quieren eliminar
 
 palancas$corregir <-  TRUE    # TRUE o FALSE
 
-palancas$nuevasvars <-  FALSE #si quiero hacer Feature Engineering manual
+palancas$nuevasvars <-  TRUE  #si quiero hacer Feature Engineering manual
 
 palancas$dummiesNA  <-  FALSE #La idea de Santiago Dellachiesa
 
-palancas$lag1   <- TRUE    #lag de orden 1
-palancas$delta1 <- FALSE#campo -  lag de orden 1 
-palancas$lag2   <- TRUE    
+palancas$lag1   <- FALSE #lag de orden 1
+palancas$delta1 <- FALSE #campo -  lag de orden 1 
+palancas$lag2   <- FALSE
 palancas$delta2 <- FALSE
-palancas$lag3   <- TRUE    
+palancas$lag3   <- FALSE
 palancas$delta3 <- FALSE
 
-palancas$lag4   <- TRUE    
+palancas$lag4   <- FALSE
 palancas$delta4 <- FALSE
 palancas$lag5   <- FALSE
 palancas$delta5 <- FALSE
@@ -49,16 +49,16 @@ palancas$delta6 <- FALSE
 palancas$promedio3  <- FALSE #promedio  de los ultimos 3 meses
 palancas$promedio6  <- FALSE
 
-palancas$minimo3  <- FALSE#minimo de los ultimos 3 meses
+palancas$minimo3  <- TRUE  #minimo de los ultimos 3 meses
 palancas$minimo6  <- FALSE
 
-palancas$maximo3  <- FALSE#maximo de los ultimos 3 meses
+palancas$maximo3  <- TRUE  #maximo de los ultimos 3 meses
 palancas$maximo6  <- FALSE
 
 palancas$ratiomax3   <- FALSE#La idea de Daiana Sparta
-palancas$ratiomean6  <- FALSE #Un derivado de la idea de Daiana Sparta
+palancas$ratiomean6  <- TRUE  #Un derivado de la idea de Daiana Sparta
 
-palancas$tendencia6  <- TRUE    #Great power comes with great responsability
+palancas$tendencia6  <- FALSE#Great power comes with great responsability
 
 palancas$canaritosimportancia  <- TRUE  #si me quedo solo con lo mas importante de canaritosimportancia
 
